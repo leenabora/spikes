358e99ceef1ea438160ed84e13608dc64f248e13
Leena <lbora@equalexperts.com>
2012-08-01 14:00:04 +0530

    clock result field is changed to clob

--- ActiveMQTest/src/main/java/com/tesco/spike/dao/impl/ClockResultJDBCDao.java
+++ ActiveMQTest/src/main/java/com/tesco/spike/dao/impl/ClockResultJDBCDao.java
@@ -4,9 +4,12 @@ import com.tesco.spike.dao.ClockResultDao;
 import com.tesco.spike.vo.ClockResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.Types;
 import java.util.List;
 
 
@@ -14,31 +17,42 @@ import java.util.List;
 public class ClockResultJDBCDao implements ClockResultDao {
 
     private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate template;
 
     @Autowired
     public void createTemplate(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        template = new NamedParameterJdbcTemplate(dataSource);
     }
 
     public String getClockResultByTransactioNo(String transactionNo) {
-        return jdbcTemplate.queryForObject(
-                "select clock_result from clockresult where transaction_no = ?",
-                String.class, transactionNo);
+        String query = "SELECT clock_result FROM clockresult WHERE transaction_no = :transactionNo";
+
+        MapSqlParameterSource paramSource = new MapSqlParameterSource();
+        paramSource.addValue("transactionNo", transactionNo, Types.VARCHAR);
+
+        return (String) template.queryForObject(query, paramSource, String.class);
     }
 
-    public List<String> getClockResultByLoyaltyCardNo(String loyaltyCardNo) {
-        String query = "select clock_result from clockresult where loyalty_card_no = ?";
-        Object[] params = {loyaltyCardNo};
+   public List<String> getClockResultByLoyaltyCardNo(String loyaltyCardNo) {
+        String query = "SELECT clock_result FROM clockresult WHERE loyalty_card_no = :loyaltyCardNo";
 
-        return jdbcTemplate.queryForList(query, String.class, params);
+        MapSqlParameterSource paramSource = new MapSqlParameterSource();
+        paramSource.addValue("loyaltyCardNo", loyaltyCardNo, Types.VARCHAR);
+
+        return  template.queryForList(query, paramSource, String.class);
     }
+
 
     @Override
     public void ingestClockResult(ClockResult clockResult) {
-        String sql = "insert into clockresult(transaction_no, loyalty_card_no, clock_result) values " +
-                "(?,?,?)";
+        String query = "INSERT INTO clockresult(transaction_no, loyalty_card_no, clock_result) VALUES (:transactionNo,:loyaltyCardNo,:clockResult)";
 
-        Object[] obj = {clockResult.getTransactionNo(), clockResult.getLoyaltyCardNo(), clockResult.getClockResultString()};
-        jdbcTemplate.update(sql, obj);
+        MapSqlParameterSource paramSource = new MapSqlParameterSource();
+        paramSource.addValue("transactionNo", clockResult.getTransactionNo(), Types.VARCHAR);
+        paramSource.addValue("loyaltyCardNo", clockResult.getLoyaltyCardNo(), Types.VARCHAR);
+        paramSource.addValue("clockResult", clockResult.getClockResultString(), Types.CLOB);
+
+        template.update(query, paramSource);
     }
 }
